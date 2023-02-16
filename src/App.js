e696d7a0425f0149b1bc8d2f5c083497e3a367db
devahmad47 <ahmadjutt4781@gmail.com>
2023-02-16 22:18:32 +0500

    react login

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+
+import Headers from "./components/Header";
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Login />
+        <Header /> 
+      
+    </div>
+  );
+}
+
+export default App;
